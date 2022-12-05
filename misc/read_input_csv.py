21cdb3f2585e480345a379858345fd7febb497f3
Kristian Fossum <krfo@norceresearch.no>
2022-12-05 09:08:51 +0100

    package containing miscellaneous tools for pipt and popt

---
+++ misc/read_input_csv.py
@@ -0,0 +1,130 @@
+"""
+File for reading CSV files and returning a 2D list
+"""
+import pandas as pd
+import numpy as np
+
+def read_data_csv(filename, datatype, truedataindex):
+    """
+    Input:
+        - filename:         Name of csv-file
+        - datatype:         List of data types as strings
+        - truedataindex:    List of where the "TRUEDATA" has been extracted (e.g., at which time, etc)
+
+    Output:
+        - imported_data:    List of observed data
+    """
+
+    df = pd.read_csv(filename)  # Read the file
+
+    imported_data = []  # Initialize the 2D list of csv data
+    tlength = len(truedataindex)
+    dnumber = len(datatype)
+
+    if df.columns[0] == 'header_both':  # csv file has column and row headers
+        pos = [None] * dnumber
+        for col in range(dnumber):
+            pos[col] = df.columns.get_loc(datatype[col])  # find index of data type in csv file header
+        for t in truedataindex:
+            row = df[df['header_both'] == t]  # pick row corresponding to truedataindex
+            row = row.values[0]  # select the values of the dataframe row
+            csv_data = [None] * dnumber
+            for col in range(dnumber):
+                try:  # Making a float
+                    csv_data[col] = float(row[pos[col]])
+                except:  # It is a string
+                    csv_data[col] = row[pos[col]]
+            imported_data.append(csv_data)
+    else:  # No row headers (the rows in the csv file must correspond to the order in truedataindex)
+        if tlength == df.shape[0]:  # File has column headers
+            pos = [None] * dnumber
+            for col in range(dnumber):
+                pos[col] = df.columns.get_loc(datatype[col])  # Find index of the header in datatype
+        elif tlength == df.shape[0]+1:  # File has no column headers (columns must correspond to the order in datatype)
+            # First row has been misinterpreted as header, so we read first row again:
+            temp = pd.read_csv(filename, header=None, nrows=1).values[0]
+            pos = list(range(df.shape[1]))  # Assume the data is in the correct order
+            csv_data = [None] * len(temp)
+            for col in range(len(temp)):
+                try:  # Making a float
+                    csv_data[col] = float(temp[col])
+                except:  # It is a string
+                    csv_data[col] = temp[col]
+            imported_data.append(csv_data)
+
+        for rows in df.values:
+            csv_data = [None] * dnumber
+            for col in range(dnumber):
+                try:  # Making a float
+                    csv_data[col] = float(rows[pos[col]])
+                except:  # It is a string
+                    csv_data[col] = rows[pos[col]]
+            imported_data.append(csv_data)
+
+    return imported_data
+
+
+def read_var_csv(filename, datatype, truedataindex):
+    """
+    Input:
+        - filename:         Name of csv-file
+        - datatype:         List of data types as strings
+        - truedataindex:    List of where the "TRUEDATA" has been extracted (e.g., at which time, etc)
+
+    Output:
+        - imported_var:    List of variances
+    """
+
+    df = pd.read_csv(filename)  # Read the file
+
+    imported_var = []  # Initialize the 2D list of csv data
+    tlength = len(truedataindex)
+    dnumber = len(datatype)
+
+    if df.columns[0] == 'header_both':  # csv file has column and row headers
+        pos = [None] * dnumber
+        for col in range(dnumber):
+            pos[col] = df.columns.get_loc(datatype[col])  # find index of data type in csv file header
+        for t in truedataindex:
+            row = df[df['header_both'] == t]  # pick row
+            row = row.values[0]  # select the values of the dataframe
+            csv_data = [None] * 2 * dnumber
+            for col in range(dnumber):
+                csv_data[2*col] = row[pos[col]]
+                try:  # Making a float
+                    csv_data[2*col+1] = float(row[pos[col]]+1)
+                except:  # It is a string
+                    csv_data[2*col+1] = row[pos[col]+1]
+            csv_data[0::2] = [x.lower() for x in csv_data[0::2]]  # Make sure the string input is lowercase
+            imported_var.append(csv_data)
+    else:  # No row headers (the rows in the csv file must correspond to the order in truedataindex)
+        if tlength == df.shape[0]:  # File has column headers
+            pos = [None] * dnumber
+            for col in range(dnumber):
+                pos[col] = df.columns.get_loc(datatype[col])  # Find index of datatype in csv file header
+        elif tlength == df.shape[0]+1:  # File has no column headers (columns must correspond to the order in datatype)
+            # First row has been misinterpreted as header, so we read first row again:
+            temp = pd.read_csv(filename, header=None, nrows=1).values[0]
+            temp[0::2] = [x.lower() for x in temp[0::2]]  # Make sure the string input is lowercase
+            pos = list(range(0, df.shape[1], 2))  # Assume the data is in the correct order
+            csv_data = [None] * len(temp)
+            for col in range(dnumber):
+                csv_data[2 * col] = temp[2 * col]
+                try:  # Making a float
+                    csv_data[2*col+1] = float(temp[2*col+1])
+                except:  # It is a string
+                    csv_data[2*col+1] = temp[2*col+1]
+            imported_var.append(csv_data)
+
+        for rows in df.values:
+            csv_data = [None] * 2 * dnumber
+            for col in range(dnumber):
+                csv_data[2*col] = rows[2*col]
+                try:  # Making a float
+                    csv_data[2*col+1] = float(rows[pos[col]+1])
+                except:  # It is a string
+                    csv_data[2*col+1] = rows[pos[col]+1]
+            csv_data[0::2] = [x.lower() for x in csv_data[0::2]]  # Make sure the string input is lowercase
+            imported_var.append(csv_data)
+
+    return imported_var
