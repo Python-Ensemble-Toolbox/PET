86dda05d3c9045c9d3208f567b02d0d2930ede4a
Kristian Fossum <krfo@norceresearch.no>
2022-12-05 09:34:27 +0100

    add master file and package init

---
+++ pipt/__init__.py
@@ -0,0 +1,13 @@
+"""Python Inverse Problem Toolbox (PIPT)
+
+.. include:: ./README.md
+"""
+
+__pdoc__ = {'plot':False,'simulator.log_gp':False, 'simulator.log':False,
+            'simulator.shallowlogs':False,'simulator.subsurf_flow':False,
+             'simulator.flow_rock':False, 'simulator.flow_rock_feat':False}
+
+# import replacement module and override submodule in pipt
+# import sys
+# from #replacement_package import #replacement_submodule
+# sys.modules["pipt.#module.#submodule"] = #replacement_submodule
---
+++ pipt/pipt_init.py
@@ -0,0 +1,13 @@
+# External imports
+from fnmatch import filter  # to check if wildcard name is in list
+from importlib import import_module
+
+def init_da(da_input,fwd_input,sim):
+    "initialize the ensemble object based on the DA inputs"
+
+    assert len(da_input['daalg']) == 2, f"Need to input assimilation type and update method, got {da_input['daalg']}"
+
+    da_import = getattr(import_module('pipt.update_schemes.' + da_input['daalg'][0]), f'{da_input["daalg"][1]}_{da_input["analysis"]}')
+
+    # Init. update scheme class, and get an object of that class
+    return da_import(da_input, fwd_input,sim)
