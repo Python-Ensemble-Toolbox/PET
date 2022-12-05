fffc5bc796c016b848d22457248642a98b870f88
Kristian Fossum <krfo@norceresearch.no>
2022-12-05 10:59:16 +0100

    update setup script

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='PET',
     version='1.0',
-    #packages=['pipt.plot', 'pipt.debug', 'pipt.fwd_sim', 'pipt.geostat', 'pipt.paramrep', 'pipt.simulator', 'pipt.misc_tools', 'pipt.rockphysics','pipt.input_output', 'pipt.optimization', 'pipt.system_tools', 'pipt.update_schemes', 'pipt.post_processing'],
+    packages=['pipt', 'popt', 'ensemble', 'simulator', 'input_output', 'misc'],
     url='https://github.com/Python-Ensemble-Toolbox/PET',
     license_files=('LICENSE.txt',),
     author='',
     author_email='krfo@norceresearch.no',
     description='Python Ensemble Toolbox',
-    install_requires=['pyresito @ git+https://github.com/NORCE-Energy/pyresito.git',
-                      'numpy', 'scipy', 'matplotlib', 'h5py', 'mako', 'tqdm', 'ray', 'PyWavelets', 
-                      'psutil', 'pdoc3', 'pytest', 'pandas', 'p_tqdm'],
+    install_requires=['pyresito @ git+ssh://git@github.com/NORCE-Energy/pyresito@master',
+                      'numpy', 'scipy', 'matplotlib', 'h5py', 'mako', 'tqdm', 'PyWavelets', 
+                      'psutil', 'pdoc3', 'pytest', 'pandas', 'p_tqdm', 'mat73'],
 )
