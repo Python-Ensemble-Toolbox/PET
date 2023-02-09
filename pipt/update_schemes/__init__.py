a91d4c0d26ba90ad382be0c37cfe92d6f3e0a928
Kristian Fossum <krfo@norceresearch.no>
2023-02-09 11:27:20 +0100

    No extra package is added

--- pipt/update_schemes/__init__.py
+++ pipt/update_schemes/__init__.py
@@ -1,4 +1,4 @@
 # Add path to private repo to access the private methods
-import os
-home = os.path.expanduser("~") # os independent home
-__path__.append(os.path.join(home,'4DSEIS_private/4DSEIS-packages/update_schemes'))
+#import os
+#home = os.path.expanduser("~") # os independent home
+#__path__.append(os.path.join(home,'4DSEIS_private/4DSEIS-packages/update_schemes'))
