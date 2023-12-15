408b1c09e853afc914571881b2513eebd5144d08
rolfjl <rolo@norceresearch.no>
2023-12-15 10:47:33 +0100

    Fix link in tutorials

--- docs/tutorials.py
+++ docs/tutorials.py
@@ -1,3 +1,4 @@
-"""
-[POPT tutorial](https://github.com/Python-Ensemble-Toolbox/PET/blob/main/docs/tutorials/popt/tutorial_popt.ipynb)
+""" Jupyter notebooks.
+
+A POPT tutorial is found [here](https://github.com/Python-Ensemble-Toolbox/PET/blob/main/docs/tutorials/popt/tutorial_popt.ipynb)
 """
