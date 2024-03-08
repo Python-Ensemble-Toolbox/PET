f05efdd0b0aafbcac74205cd40e0c4f913284775
Mathias Methlie Nilsen <mani@bgo-1714.ad.norceresearch.no>
2024-03-08 14:37:07 +0100

    made it much more efficient

--- popt/cost_functions/ren_npv_co2.py
+++ popt/cost_functions/ren_npv_co2.py
@@ -2,18 +2,16 @@
 
 import pandas as pd
 import numpy  as np
-import sys
 import os
+import yaml
 
-from scipy.interpolate import interp1d
 from pathlib import Path
-from libecalc.application.energy_calculator import EnergyCalculator
-from libecalc.common.time_utils import Frequency
-from libecalc.presentation.yaml.model import YamlModel
+from pqdm.processes import pqdm
+
+__all__ = ['ren_npv_co2']
 
 HERE = Path().cwd()  # fallback for ipynb's
 HERE = HERE.resolve()
-
 
 def ren_npv_co2(pred_data, keys_opt, report, save_emissions=False):
     '''
@@ -34,9 +32,14 @@ def ren_npv_co2(pred_data, keys_opt, report, save_emissions=False):
     -------
     objective_values : array_like
         Objective function values (NPV) for all ensemble members.
-    
-    NOTE: This function is not very generalized!
     '''
+
+    # some globals, for pqdm
+    global const
+    global kwargs
+    global report_dates
+    global sim_data
+
     # define a data getter
     get_data = lambda i, key: pred_data[i+1][key].squeeze() - pred_data[i][key].squeeze()
 
@@ -47,132 +50,144 @@ def ren_npv_co2(pred_data, keys_opt, report, save_emissions=False):
     except: 
         ne = 1
 
+    np.save('co2_emissions', np.zeros((ne, nt-1)))
+
     # Economic and other constatns
     const  = dict(keys_opt['npv_const'])
-    kwargs = dict(keys_opt['npv_kwargs']) 
+    kwargs = dict(keys_opt['npv_kwargs'])
+    report_dates = report[1]
 
-    # define some empty  array variables
-    prod_oil, prod_gas, prod_wat, inj_wat, thp = [np.zeros((ne, nt-1)) for _ in range(5)]
-    ndays = np.zeros(nt-1)
-    npv_values = np.zeros(ne)
+    # Load energy arrays. These arrays contain the excess windpower used for gas compression,
+    # and the energy from gas which is used in the water intection.
+    power_arrays = np.load(kwargs['power']+'.npz')
+
+    sim_data = {'fopt': np.zeros((ne, nt-1)),
+                'fgpt': np.zeros((ne, nt-1)),
+                'fwpt': np.zeros((ne, nt-1)),
+                'fwit': np.zeros((ne, nt-1)),
+                'thp' : np.zeros((ne, nt-1)),
+                'days': np.zeros(nt-1),
+                'wind': power_arrays['wind'][:,:-1]}
 
     # loop over pred_data
     for t in range(nt-1):
-
-        # field data
-        prod_oil[:,t] = get_data(t, 'fopt')
-        prod_gas[:,t] = get_data(t, 'fgpt')
-        prod_wat[:,t] = get_data(t, 'fwpt')
-        inj_wat[:,t]  = get_data(t, 'fwit')
-
+        
+        for datatype in ['fopt', 'fgpt', 'fwpt', 'fwit']:
+            sim_data[datatype][:,t] = get_data(t, datatype)
+        
         # days in time-step
-        ndays[t] = (report[1][t+1] - report[1][t]).days
+        sim_data['days'][t] = (report_dates[t+1] - report_dates[t]).days
 
         # get maximum well head pressure (for each ensemble member)
         thp_keys = [k for k in keys_opt['datatype'] if 'wthp' in k] # assume only injection wells
         thp_vals = []
         for key in thp_keys:
             thp_vals.append(pred_data[t][key].squeeze())
-        thp[:,t] = np.max(np.array(thp_vals), axis=0)
+            
+        sim_data['thp'][:,t] = np.max(np.array(thp_vals), axis=0)
 
-    # Load energy arrays. These arrays contain the excess windpower used for gas compression,
-    # and the energy from gas which is used in the water intection.
-    power_arrays = np.load(kwargs['power']+'.npz')
-    ren_comp_energy = power_arrays['ren']
-    gas_inj_energy  = power_arrays['gas']
+    # calculate NPV values 
+    npv_values = pqdm(array=range(ne), function=npv, n_jobs=keys_opt['parallel'], disable=True)
 
-    # calculate the NPV values
-    emissions_ensemble = []
-    for n in range(ne):
-
-        # config eCalc
-        pd.DataFrame( {'dd-mm-yyyy' : report[1][1:],
-                       'OIL_PROD'   : prod_oil[n]/ndays,
-                       'GAS_PROD'   : prod_gas[n]/ndays, 
-                       'WATER_INJ'  : inj_wat[n]/ndays,
-                       'THP_MAX'    : thp[n]} ).to_csv('ecalc_input.csv', index=False)
-        
-        model_path = HERE/str(kwargs['yamlfile']+'.yaml')
-        yaml_model = YamlModel(path=model_path, output_frequency=Frequency.NONE)
-        
-        # compute power consumption of gas compressor with eCalc 
-        model = EnergyCalculator(graph=yaml_model.graph)
-        consumption = results_as_df(yaml_model, 
-                                    model.evaluate_energy_usage(yaml_model.variables), 
-                                    lambda r: r.component_result.energy_usage)
-
-        base_power  = consumption[kwargs['basekeys']].sum(axis=1).values
-        total_gas_power = base_power - ren_comp_energy[n,:-1] + gas_inj_energy[n,:-1]
-
-
-        # calculate the co2 emissions. This is done the same way as eCalc does it,
-        # first we interpolate the genset table
-        gen_df    = remove_comments_from_df(pd.read_csv('genset.csv'))
-        gen_pow   = gen_df[gen_df.columns[0]].values
-        gen_fuel  = gen_df[gen_df.columns[1]].values
-        gen_curve = interp1d(x=gen_pow, y=gen_fuel)
-
-        fuel = gen_curve(total_gas_power)                   # Sm3/day
-        fuel = fuel*ndays                                   # Sm3 
-        co2_emission_factor = 2.416                         # kg/Sm3
-        co2_emissions = co2_emission_factor*fuel/1000       # tons
-
-        if save_emissions:
-            emissions_ensemble.append(co2_emissions)
-    
-        # calculate npv value
-        gain = const['wop']*prod_oil[n] + const['wgp']*prod_gas[n]
-        loss = const['wwp']*prod_wat[n] + const['wwi']*inj_wat[n] + const['wem']*co2_emissions
-        disc = (1+const['disc'])**(ndays/365)
-
-        npv_values[n] = np.sum( (gain-loss)/disc )
-    
-    if save_emissions:
-        emissions_ensemble = np.array(emissions_ensemble)
-        np.save('co2_emissions.npy', emissions_ensemble)
+    if not save_emissions:
+        os.remove('co2_emissions.npy')
 
     # clear energy arrays
-    np.savez(kwargs['power']+'.npz', 
-             ren=np.zeros_like(ren_comp_energy), 
-             gas=np.zeros_like(gas_inj_energy))
-
-    # delete ecalc_input
-    os.remove('ecalc_input.csv')  
+    np.savez(kwargs['power']+'.npz', wind=np.zeros((ne, nt)))
 
     scaling = 1.0
     if 'obj_scaling' in const:
         scaling = const['obj_scaling']
     
-    return npv_values/scaling
+    return np.asarray(npv_values)/scaling
 
 
-def results_as_df(yaml_model, results, getter) -> pd.DataFrame:
-    '''Extract relevant values, as well as some meta (`attrs`).'''
-    df = {}
-    attrs = {}
-    res = None
-    for id_hash in results:
-        res = results[id_hash]
-        res = getter(res)
-        component = yaml_model.graph.get_node(id_hash)
-        df[component.name] = res.values
-        attrs[component.name] = {'id_hash': id_hash,
-                                 'kind': type(component).__name__,
-                                 'unit': res.unit}
-    if res is None:
-        sys.exit('No emission results from eCalc!')
-    df = pd.DataFrame(df, index=res.timesteps)
-    df.index.name = "dates"
-    df.attrs = attrs
-    return df
+def emissions(yaml_file="ecalc_config.yaml"):
 
-def remove_comments_from_df(dataframe: pd.DataFrame, symbol='#') -> pd.DataFrame:
-    ''' Remove all rows with symbol in them'''
-    cols = dataframe.columns
-    for i in dataframe.index:
-        if symbol in dataframe[cols].loc[i].to_string():
-            dataframe = dataframe.drop(index=[i])
-    return dataframe.astype('float64')
+    from libecalc.application.energy_calculator import EnergyCalculator
+    from libecalc.common.time_utils import Frequency
+    from libecalc.presentation.yaml.model import YamlModel
+
+    # Config
+    model_path = HERE / yaml_file
+    yaml_model = YamlModel(path=model_path, output_frequency=Frequency.NONE)
+
+    # Compute energy, emissions
+    model = EnergyCalculator(graph=yaml_model.graph)
+    consumer_results = model.evaluate_energy_usage(yaml_model.variables)
+    emission_results = model.evaluate_emissions(yaml_model.variables, consumer_results)
+
+    # print power from pump
+    co2 = []
+    for identity, component in yaml_model.graph.nodes.items():
+        if identity in emission_results:
+            co2.append(emission_results[identity]['co2_fuel_gas'].rate.values)
+            
+    co2 = np.sum(np.asarray(co2), axis=0)
+    return co2
+
+
+def npv(n):
+       
+    days = sim_data['days']
+
+    # config eCalc
+    pd.DataFrame( {'dd-mm-yyyy'  : report_dates[1:],
+                   'OIL_PROD'   : sim_data['fopt'][n]/days,
+                   'GAS_PROD'   : sim_data['fgpt'][n]/days, 
+                   'WATER_INJ'  : sim_data['fwit'][n]/days,
+                   'THP_MAX'    : sim_data['thp'][n],
+                   'WIND_POWER' : sim_data['wind'][n]*(-1)
+                } ).to_csv(f'ecalc_input_{n}.csv', index=False)
+    
+    ecalc_yaml_file = kwargs['yamlfile']+'.yaml'
+    new_yaml = duplicate_yaml_file(filename=ecalc_yaml_file, member=n)
+    
+    #calc emissions
+    co2 = emissions(new_yaml)*days
+
+    # save emissions
+    try:
+        en_co2 = np.load('co2_emissions.npy')
+        en_co2[n] = co2
+        np.save('co2_emissions', en_co2)
+    except:
+        import time
+        time.sleep(1)
+
+    #calc npv
+    gain = const['wop']*sim_data['fopt'][n] + const['wgp']*sim_data['fgpt'][n]
+    loss = const['wwp']*sim_data['fwpt'][n] + const['wwi']*sim_data['fwit'][n] + const['wem']*co2
+    disc = (1+const['disc'])**(days/365)
+
+    npv_value = np.sum( (gain-loss)/disc )
+
+    # delete dummy files
+    os.remove(new_yaml)
+    os.remove(f'ecalc_input_{n}.csv')
+
+    return npv_value
+
+
+def duplicate_yaml_file(filename, member):
+
+    try:
+        # Load the YAML file
+        with open(filename, 'r') as yaml_file:
+            data = yaml.safe_load(yaml_file)
+
+        input_name = data['TIME_SERIES'][0]['FILE']
+        data['TIME_SERIES'][0]['FILE'] = input_name.replace('.csv', f'_{member}.csv')
+
+        # Write the updated content to a new file
+        new_filename = filename.replace(".yaml", f"_{member}.yaml")
+        with open(new_filename, 'w') as new_yaml_file:
+            yaml.dump(data, new_yaml_file, default_flow_style=False)
+
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+    return new_filename
 
 
 
