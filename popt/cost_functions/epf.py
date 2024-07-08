b8f55624e252cf615971cf2a4123b7614359a6dd
rolfjl <rolo@norceresearch.no>
2024-07-08 13:34:36 +0200

    Fix equation in docstring

--- popt/cost_functions/epf.py
+++ popt/cost_functions/epf.py
@@ -4,7 +4,7 @@ def epf(r, c_eq=0, c_iq=0):
     r"""
     The external penalty function, given as
     .. math::
-        0.5r(\sum(c_eq^2) + \sum(\max(c_iq,0)^2)
+        0.5r(\sum_i(c_{eq}^2) + \sum(\max(c_{iq},0)^2)
     """
     
     return r*0.5*( np.sum(c_eq**2) + np.sum(np.maximum(-c_iq,0)**2) )
