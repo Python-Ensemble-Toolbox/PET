d1abf36227f2365d18d7c264ed5beeb12978cb1c
Mathias Methlie Nilsen <mani@bgo-1714.ad.norceresearch.no>
2024-07-09 08:42:48 +0200

    added loop over Keras optimizers

--- popt/update_schemes/popt/update_schemes/linesearch/LineSearch/calc_update/<locals>/_jac/func_code.py
+++
@@ -1,7 +0,0 @@
-# first line: 89
-        @memory.cache
-        def _jac(x):
-            g = self.jac(x, self.cov)
-            if self.normalize:
-                g = g/la.norm(g, np.inf)
-            return g
