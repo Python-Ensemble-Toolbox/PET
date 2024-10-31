9b96c10c42758199bf90979b617d78264814476a
Mathias Methlie Nilsen <mathias.m.nilsen97@gmail.com>
2024-10-31 10:11:22 +0100

    updated LineSearch

--- popt/update_schemes/linesearch.py
+++ popt/update_schemes/linesearch.py
@@ -5,7 +5,7 @@ import pprint
 import warnings
 
 from numpy import linalg as la
-from scipy.optimize import line_search
+#from scipy.optimize import line_search
 
 # Internal imports
 from popt.misc_tools import optim_tools as ot
@@ -99,18 +99,19 @@ class LineSearch(Optimize):
             
         # Perform Line Search
         self.logger.info('Performing line search...')
-        line_search_kwargs = {'f'        : _fun,
-                              'myfprime' : _jac,
-                              'xk'       : self.mean_state,
-                              'pk'       : -pk/pk_norm,
-                              'gfk'      : pk,
-                              'old_fval' : self.obj_func_values.mean(),
-                              'c1'       : self.ls_options['c1'],
-                              'c2'       : self.ls_options['c2'],
-                              'amax'     : self.alpha_max,
-                              'maxiter'  : self.alpha_iter_max}
+        ls_kw = {'fun': _fun,
+                 'grad': _jac,
+                 'amax': self.alpha_max,
+                 'xk': self.mean_state,
+                 'pk': -pk/pk_norm,
+                 'fk': self.obj_func_values.mean(),
+                 'gk': pk,
+                 'c1': self.ls_options['c1'],
+                 'c2': self.ls_options['c2'],
+                 'maxiter': self.alpha_iter_max,
+                 'logger': self.logger}
         
-        step_size, _, _, fnew, _, slope = line_search(**line_search_kwargs)
+        step_size, fnew, gnew, alpha_iter = line_search_step(**ls_kw)
         
         if isinstance(step_size, float):
             self.logger.info('Strong Wolfie conditions satisfied')
@@ -119,7 +120,7 @@ class LineSearch(Optimize):
             self.mean_state      = ot.clip_state(self.mean_state - step_size*pk/pk_norm, self.bounds)
             self.obj_func_values = fnew
             self.alpha           = step_size
-            self.pk_from_ls      = slope
+            self.pk_from_ls      = gnew
 
             # Update covariance 
             #TODO: This sould be mande into an callback function for generality 
@@ -135,7 +136,7 @@ class LineSearch(Optimize):
             # Write logging info
             if self.logger is not None:
                 info_str_iter = '       {:<10} {:<10} {:<15.4e} {:<15.2e} {:<15.2e}'.\
-                    format(self.iteration, 0, np.mean(self.obj_func_values),
+                    format(self.iteration, alpha_iter, np.mean(self.obj_func_values),
                             self.alpha, self.cov[0, 0])
                 self.logger.info(info_str_iter)
 
@@ -162,3 +163,124 @@ class LineSearch(Optimize):
     
         return success
 
+
+def line_search_step(fun, grad, amax, xk, pk, fk=None, gk=None, args=(), c1=0.0001, c2=0.9, maxiter=5, **kwargs):
+    '''
+    Perform a line search to find an acceptable step size that satisfies the Armijo and curvature conditions.
+
+    Parameters
+    -----------------------------------------------------------------------------------------------------------------------------
+    fun : callable
+        The objective function to be minimized.
+    grad : callable
+        The gradient of the objective function.
+    amax : float
+        The maximum step size.
+    xk : ndarray
+        The current point.
+    pk : ndarray
+        The search direction.
+    fk : float, optional
+        The value of the objective function at xk. If None, it will be computed.
+    gk : ndarray, optional
+        The gradient of the objective function at xk. If None, it will be computed.
+    args : tuple, optional
+        Additional arguments passed to the objective function and its gradient.
+    c1 : float, optional
+        The Armijo condition constant. Default is 0.0001.
+    c2 : float, optional
+        The curvature condition constant. Default is 0.9. If None, the curvature condition is ignored.
+    maxiter : int, optional
+        The maximum number of iterations. Default is 5.
+    **kwargs : dict, optional
+        Additional keyword arguments. 'rho' can be used to specify the step size reduction factor. Default is 0.5.
+
+    Returns
+    -----------------------------------------------------------------------------------------------------------------------------
+    step_size : float
+        The step size that satisfies the conditions.
+    fnew : float
+        The value of the objective function at the new point.
+    gnew : ndarray
+        The gradient of the objective function at the new point.
+    i : int
+        The number of iterations performed.
+
+    Notes
+    -----------------------------------------------------------------------------------------------------------------------------
+    The function performs a backtracking line search to find a step size that satisfies both the Armijo and curvature conditions.
+    If the conditions are not satisfied within the maximum number of iterations, the function returns None for the step size.
+    If c2 is None, the curvature condition is ignored, and only the Armijo condition is checked.
+    '''
+    # Compute the function value and gradient at the current point if not provided
+    if fk is None:
+        fk = fun(xk, *args)
+    if gk is None:
+        gk = grad(xk, *args)
+
+    # Assert that the constants c1 and c2 are within valid ranges
+    assert 0.0 <= c1, '0 <= c1'
+    if c2 is not None: 
+        assert 0.0 <= c2 < 1.0, '0 <= c2 < 1'
+
+    # Check for logger
+    logger = kwargs.get('logger', None)
+
+    # Initialize conditions and step size
+    step_size = amax
+    fnew = None
+    gnew = None
+    rho  = kwargs.get('rho', 0.5)  # Step size reduction factor
+
+    # Perform the line search
+    for i in range(maxiter+1):   
+        armijo = False
+        curvature = False
+
+        # Compute the function value at the new point
+        fnew = fun(xk + step_size*pk)
+
+        # Check the Armijo condition
+        if fnew <= fk + c1*step_size*np.dot(pk, gk):
+            armijo = True
+            _log(logger, message='Armijo condition: Satisfied')
+
+            # Check the Curvature condition if c2 is not None
+            if c2 is None:
+                curvature = True
+            else:
+                gnew = grad(xk + step_size*pk)  
+                if np.abs(np.dot(pk, gnew)) <= c2*np.abs(np.dot(pk, gk)):
+                    curvature = True
+                    _log(logger, message='Curvature condition: Satisfied')
+        
+        # If both conditions are satisfied, return the step size and new values
+        if armijo and curvature:
+            return step_size, fnew, gnew, i
+        else:
+            if not armijo:
+                _log(logger, message='Armijo condition: Failed')
+            if not curvature and c2 is not None:
+                _log(logger, message='Curvature condition: Failed')
+
+            # Reduce the step size
+            step_size = rho*step_size
+
+    # If the line search did not converge, print a message and return NaN
+    _log(logger, message='Line Search did not converge')
+    
+    return None, fnew, gnew, i
+
+def _log(logger, message):
+    if logger is None:
+        print(message)
+    else:
+        logger.info(message)
+
+
+
+
+
+
+
+    
